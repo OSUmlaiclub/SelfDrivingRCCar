9dd182c86ac212d0664dcfcab32a5e40b0aa2cde
Mazen Alotaibi <mazen.ota@gmail.com>
2018-05-09 19:22:02 -0700

    first draft of the ui

---
+++ ui/public/js/connection.js
@@ -0,0 +1,79 @@
+// make connection
+var socket = io.connect('http://localhost:8000');
+
+// query DOM
+var liveFeed = document.getElementById('live-feed'),
+  startBtn = document.forms[0],
+  onBtn = document.getElementById('on-btn'),
+  offBtn = document.getElementById('off-btn'),
+  accuractionRate = document.forms[1],
+  moveFront = document.getElementById('move-front'),
+  moveBack = document.getElementById('move-back'),
+  moveLeft = document.getElementById('move-left'),
+  moveRight = document.getElementById('move-right');
+
+console.log(startBtn.on);
+// emit events
+startBtn.on.addEventListener('click', function() {
+  console.log('sent value (on)', this.value);
+  socket.emit('startBtn', {
+    switch: this.value
+  });
+  console.log('data sent to server (startBtn)');
+});
+startBtn.off.addEventListener('click', function() {
+  console.log('sent valu (off)', this.value);
+  socket.emit('startBtn', {
+    switch: this.value
+  });
+  console.log('data sent to server (startBtn)');
+});
+accuractionRate.rate.addEventListener('click', function() {
+  socket.emit('rate', {
+    rate: 1
+  });
+  console.log('data sent to server (rate)');
+});
+moveFront.addEventListener('click', function() {
+  socket.emit('move', {
+    move: 1
+  });
+  console.log('data sent to server (moveFront)');
+});
+moveFront.addEventListener('click', function() {
+  socket.emit('move', {
+    move: 1
+  });
+  console.log('data sent to server (moveFront)');
+});
+moveBack.addEventListener('click', function() {
+  socket.emit('move', {
+    move: 2
+  });
+  console.log('data sent to server (moveBack)');
+});
+moveLeft.addEventListener('click', function() {
+  socket.emit('move', {
+    move: 3
+  });
+  console.log('data sent to server (moveLeft)');
+});
+moveRight.addEventListener('click', function() {
+  socket.emit('move', {
+    move: 4
+  });
+  console.log('data sent to server (moveRight)');
+});
+
+socket.on('update', function(data) {
+  // set on and off button
+  //console.log('data reseved from server', data.switch);
+  onBtn.checked = false;
+  offBtn.checked = false;
+  if (data.switch == 1) offBtn.checked = true;
+  else if (data.switch == 0) onBtn.checked = true;
+
+  // set accuractionRate
+
+  accuractionRate.value = data.rate;
+});
---
+++ ui/server.js
@@ -0,0 +1,76 @@
+// server
+'use strict';
+var fs = require('fs'),
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  http_module = require('http'),
+  socket = require('socket.io'),
+  path = require('path'),
+  app = express(),
+  http = http_module.Server(app);
+app.set('port', process.env.PORT || 8000);
+
+// reading json files
+//var currentPath = process.cwd();
+//var dataFolder = currentPath + '/data/';
+//var aboutJSON = JSON.parse(fs.readFileSync(dataFolder + 'about.json', 'utf8'));
+//var blogJSON = JSON.parse(fs.readFileSync(dataFolder + 'blog.json', 'utf8'));
+//var labJSON = JSON.parse(fs.readFileSync(dataFolder + 'lab.json', 'utf8'));
+//var cvJSON = JSON.parse(fs.readFileSync(dataFolder + 'cv.json', 'utf8'));
+
+// View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Set Static Fold
+app.use('/', express.static(__dirname + '/public/'));
+
+// Define routes
+app.get('/', function(req, res) {
+  res.render('index');
+});
+app.get('*', function(req, res) {
+  res.render('404');
+});
+
+// load page
+var server = http.listen(app.get('port'), () => {
+  console.info('==> 🌎  Go to http://localhost:%s', app.get('port'));
+});
+
+// start holders
+var message = {
+  display: 0,
+  switch: 1,
+  rate: 3,
+  move: 0
+};
+// web socket
+var io = socket(server);
+io.on('connection', function(socket) {
+  console.log('made socket connection');
+
+  io.sockets.emit('update', message);
+  console.log('data sent to client (Fresh)');
+
+  socket.on('startBtn', function(data) {
+    console.log('reseved value', data.switch);
+    message.switch = data.switch;
+    io.sockets.emit('update', message);
+    console.log('data sent to client (startBtn)');
+  });
+
+  socket.on('rate', function(data) {
+    console.log('reseved value', data.rate);
+    message.rate = data.rate;
+    io.sockets.emit('update', message);
+    console.log('data sent to client (rate)');
+  });
+
+  socket.on('move', function(data) {
+    console.log('reseved value', data.move);
+    message.rate = data.move;
+    io.sockets.emit('update', message);
+    console.log('data sent to client (move)');
+  });
+});
